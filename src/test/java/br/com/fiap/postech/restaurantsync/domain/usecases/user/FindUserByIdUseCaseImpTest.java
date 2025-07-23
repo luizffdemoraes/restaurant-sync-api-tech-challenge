fdcae3f6766151f1221ef23209833723a7547384
luizffdemoraes <lffm1994@gmail.com>
2025-07-22 23:19:37 -0300

    implement: unit test user use case

---
+++ src/test/java/br/com/fiap/postech/restaurantsync/domain/usecases/user/FindUserByIdUseCaseImpTest.java
@@ -0,0 +1,57 @@
+package br.com.fiap.postech.restaurantsync.domain.usecases.user;
+
+import br.com.fiap.postech.restaurantsync.domain.entities.User;
+import br.com.fiap.postech.restaurantsync.domain.gateways.UserGateway;
+import br.com.fiap.postech.restaurantsync.infrastructure.application.dtos.responses.UserResponse;
+import br.com.fiap.postech.restaurantsync.infrastructure.exceptions.BusinessException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class FindUserByIdUseCaseImpTest {
+    @InjectMocks
+    private FindUserByIdUseCaseImp findUserByIdUseCaseImp;
+
+    @Mock
+    private UserGateway userGateway;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        user = new User("Test User", "user@test.com", "testLogin", "testPassword", null);
+        user.setId(1);
+    }
+
+    @Test
+    void execute_shouldReturnUserResponse_whenUserFoundAndAuthorized() {
+        when(userGateway.findUserById(1)).thenReturn(user);
+        doNothing().when(userGateway).validateSelfOrAdmin(user.getId());
+
+        UserResponse response = findUserByIdUseCaseImp.execute(1);
+
+        assertNotNull(response);
+        assertEquals(1, response.id());
+        verify(userGateway).findUserById(1);
+        verify(userGateway).validateSelfOrAdmin(user.getId());
+    }
+
+    @Test
+    void execute_shouldThrowException_whenNotAuthorized() {
+        when(userGateway.findUserById(1)).thenReturn(user);
+        doThrow(new BusinessException("Não autorizado")).when(userGateway).validateSelfOrAdmin(user.getId());
+
+        BusinessException exception = assertThrows(BusinessException.class, () ->
+                findUserByIdUseCaseImp.execute(1)
+        );
+        assertEquals("Não autorizado", exception.getMessage());
+        verify(userGateway).findUserById(1);
+        verify(userGateway).validateSelfOrAdmin(user.getId());
+    }
+}
